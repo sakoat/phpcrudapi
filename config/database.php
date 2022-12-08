7d2c54b85ccdb28bfd6716ac8f8d7f7dd78c4da8
sakoat07 <sakoatcse@gmail.com>
2022-12-08 22:37:05 +0600

    first commit for basic crud manager for api

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * Help  link: https://www.positronx.io/create-simple-php-crud-rest-api-with-mysql-php-pdo/
+ * PHP Basic CRUD For Rest api
+ *  we use Employee DB Table as example 
+ */
+    class Database {
+        private $host = "127.0.0.1";
+        private $database_name = "phpapidb";
+        private $username = "root";
+        private $port = "3308";
+        private $password = "";
+        public $conn;
+        public function getConnection(){
+            $this->conn = null;
+            try{
+                $this->conn = new PDO("mysql:host=" . $this->host . "; port=". $this->port."; dbname=" . $this->database_name, $this->username, $this->password);
+                $this->conn->exec("set names utf8");
+            }catch(PDOException $exception){
+                echo "Database could not be connected: " . $exception->getMessage();
+            }
+            return $this->conn;
+        }
+    }  
+?>
